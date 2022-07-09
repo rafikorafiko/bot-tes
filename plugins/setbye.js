5d8e270a97c59897e29f244af76afa95b7017b98
rafikorafiko <78249003+rafikorafiko@users.noreply.github.com>
2022-07-09 15:58:28 +0700

    Update setbye.js

--- plugins/setbye.js
+++ plugins/setbye.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text }) => {
   if (text) {
     conn.welcome = text
     m.reply('Bye berhasil diatur\n@user (Mention)')
-  } else throw 'Teksnya mana?'
+  } else throw 'Dadah korban gosting'
 }
 handler.help = ['setbye <teks>']
 handler.tags = ['owner']
