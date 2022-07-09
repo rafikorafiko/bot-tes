ec84e11dbdd93ad2afc878fa49b33613ee0430d9
rafikorafiko <78249003+rafikorafiko@users.noreply.github.com>
2022-07-09 16:02:51 +0700

    Update setwelcome.js

--- plugins/setwelcome.js
+++ plugins/setwelcome.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text }) => {
   if (text) {
     conn.welcome = text
     m.reply('Welcome berhasil diatur\n@user (Mention)')
-  } else throw 'Teksnya mana?'
+  } else throw 'welcome beban keluarga🤗'
 }
 handler.help = ['setwelcome <teks>']
 handler.tags = ['owner']
